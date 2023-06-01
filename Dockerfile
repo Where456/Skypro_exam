e5d588cfd60478a049ee00ef4731a7bf5b2321e3
Where456 <daanagrigoreva1@gmail.com>
2023-06-01 10:53:48 +0300

    commit

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,3 @@ COPY . .
 COPY docker_config.py default_config.py
 
 CMD flask run -h 0.0.0.0 -p 80
-
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from marshmallow import Schema, fields
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
@@ -11,16 +10,20 @@ app.config.from_envvar("APP_SETTINGS", silent=True)
 db = SQLAlchemy(app)
 
 
-class Note(db.Model):
+class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(255))
-    date_added = db.Column(db.DateTime, default=func.now())
+    course_title = db.Column(db.String(255))
+    data = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    office_number = db.Column(db.String(255))
 
 
-class NoteSchema(Schema):
+class ScheduleSchema(Schema):
     id = fields.Integer(dump_only=True)
-    text = fields.String(required=True, max_length=255)
-    date_added = fields.DateTime(dump_only=True)
+    course_title = fields.String(required=True, max_length=255)
+    data = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    office_number = db.Column(db.String(255))
 
 
 with app.app_context():
@@ -28,92 +31,102 @@ with app.app_context():
 
 
 @app.route("/")
-def get_note():
+def get_schedule():
     """
         Получает все заметки из базы данных и возвращает их в формате JSON.
 
         Returns:
             JSON: Список заметок в формате JSON.
     """
-    notes = Note.query.all()
-    note_data = []
-    for note in notes:
-        note_data.append({
-            'id': note.id,
-            'text': note.text,
-            'date_added': note.date_added
+    data = Schedule.query.all()
+    schedule_data = []
+    for d in data:
+        schedule_data.append({
+            'id': d.id,
+            'course_title': d.course_title,
+            'data': d.data,
+            'name': d.name,
+            'office_number': d.office_number
         })
-    return jsonify(note_data)
+    return jsonify(schedule_data)
 
 
 @app.route("/add", methods=["POST"])
-def add_note():
+def add_schedule():
     """
-        Добавляет новую заметку в базу данных.
+        Добавляет новую информацию в базу данных.
 
         Returns:
             JSON: Результат операции добавления.
     """
-    note_data = request.json
-    if not note_data or "text" not in note_data:
+    schedule_data = request.json
+    if not schedule_data or "course_title" not in schedule_data:
         return jsonify({"error": "invalid_request"}), 400
 
     try:
-        note = Note(
-            text=note_data["text"],
+        schedule = Schedule(
+            course_title=schedule_data["course_title"],
+            data=schedule_data["data"],
+            name=schedule_data["name"],
+            office_number=schedule_data["office_number"],
         )
-        db.session.add(note)
+        db.session.add(schedule)
         db.session.commit()
     except IntegrityError:
         return jsonify({"error": "already_exists"}), 400
 
-    return jsonify({"text": note.text}), 200
+    return jsonify({"course_title": schedule.course_title}), 200
 
 
-@app.route('/delete/<int:note_id>', methods=['DELETE'])
-def delete_note(note_id):
+@app.route('/delete/<int:schedule_id>', methods=['DELETE'])
+def delete_schedule(schedule_id):
     """
         Удаляет заметку из базы данных по указанному идентификатору.
 
         Args:
-            note_id (int): Идентификатор заметки.
+            schedule_id (int): Идентификатор заметки.
 
         Returns:
             str: Результат операции удаления.
     """
-    note = Note.query.get(note_id)
+    data = Schedule.query.get(schedule_id)
 
-    if note:
-        db.session.delete(note)
+    if data:
+        db.session.delete(data)
         db.session.commit()
-        return f"Note with ID {note_id} has been deleted."
+        return f"Data with ID {schedule_id} has been deleted."
     else:
-        return f"Note with ID {note_id} does not exist."
+        return f"Data with ID {schedule_id} does not exist."
 
 
-@app.route('/notes/<int:note_id>', methods=['PUT'])
-def update_note(note_id):
+@app.route('/schedules/<int:schedule_id>', methods=['PUT'])
+def update_schedule(schedule_id):
     """
         Обновляет заметку в базе данных по указанному идентификатору.
 
         Args:
-            note_id (int): Идентификатор заметки.
+            schedule_id (int): Идентификатор заметки.
 
         Returns:
             str: Результат операции обновления.
     """
-    note = Note.query.get(note_id)
-    if note:
-        new_title = request.json.get('text')
+    data = Schedule.query.get(schedule_id)
+    if data:
+        new_course_title = request.json.get('course_title')
+        new_data = request.json.get('data')
+        new_name = request.json.get('name')
+        new_office_number = request.json.get('new_office_number')
 
-        note.text = new_title if new_title else note.text
+        data.course_title = new_course_title if new_course_title else data.course_title
+        data.data = new_data if new_data else data.data
+        data.name = new_name if new_name else data.name
+        data.office_number = new_office_number if new_office_number else data.office_number
 
         db.session.commit()
-        return f"Note with ID {note_id} has been updated."
+        return f"Data with ID {schedule_id} has been updated."
     else:
-        return f"Note with ID {note_id} does not exist."
+        return f"Data with ID {schedule_id} does not exist."
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
